e82c39662226225dccb16a538efa522677759769
vinicios <vinicompif@gmail.com>
2021-03-25 16:59:12 -0300

    inicio da aula sobre checkbox

--- src/App.js
+++ src/App.js
@@ -1,42 +1,9 @@
 import React from 'react'
-import useFetch from './useFetch';
-import useLocalStorage from './useLocalStorage';
 
 const App = () => {
-
-  const [produto, setProduto] = useLocalStorage('produto', 'notebook');
-  const {request, data, loading, error} = useFetch();
-
- 
-
-  React.useEffect(()=>{
-    async function fetchData(){
-      const {response, json} = await request('https://jsonplaceholder.typicode.com/comments')
-      console.log(json);
-    }
-
-    fetchData();
-    
-  }, [request])
-
-  if(error) return <p>{error}</p>
- 
-  if(loading) return <p>Carregando...</p>
-
-  if(data){
-    return (
-      <div>
-        {data.map((comentario)=>{
-          return(
-            <h1 key={comentario.id}>{comentario.name}</h1>
-          )
-        })}
-      </div>
-    )
-  }else{
-    return null;
-  }
-  
+  return (
+    <form action=""></form>
+  )
 }
 
 export default App
