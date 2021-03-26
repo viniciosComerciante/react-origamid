7cc5e7413c895e789fff28373a97b7b1972600c5
vinicios <vinicompif@gmail.com>
2021-03-26 19:16:54 -0300

    iniciado desafio final sobre custom hooks

--- src/App.js
+++ src/App.js
@@ -1,51 +1,9 @@
 import React from 'react'
-import Input from './Form/Input';
 
-const App = () => {
-
-  const [cep, setCep] = React.useState('');
-  const [error, setError] = React.useState(null);
-
-  function validateCep(value){
-    if(value.length === 0){
-      setError('Preencha um valor')
-      return false;
-    }else if(/^\d{5}-?\d{3}$/.test(value)){
-      setError(null)
-      return true;
-    }else{
-      setError('preencha o campo corretamente')
-      return false
-    }
-  }
-
-  function handleBlur({target}){
-    validateCep(target.value);
-  }
-
-  function handleChange({target}){
-    if(error){
-      validateCep(target.value)
-    }
-    setCep(target.value)
-  }
-
-  function handleSubmit(e){
-    e.preventDefault();
-    if(validateCep(cep)){
-      console.log('enviou')
-    }else{
-      console.log('não enviar')
-    }
-  }
-
+export const App = () => {
   return (
-    <form onSubmit={handleSubmit}>
-      <Input type="text" label="cep" id="cep" placeholder="00000-000" value={cep} onChange={handleChange} onBlur={handleBlur}></Input>
-      <p style={{color: 'red'}}>{error}</p>
-      <button>Enviar</button>
-    </form>
+    <div>
+      
+    </div>
   )
 }
-
-export default App
