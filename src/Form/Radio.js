810883393351f875672022ee675a7d736def1014
vinicios <vinicompif@gmail.com>
2021-03-27 01:26:50 -0300

    terminado exercicio sobre formularios

--- src/Form/Radio.js
+++ src/Form/Radio.js
@@ -1,24 +1,51 @@
-import React from "react";
+// import React from "react";
 
-const Radio = ({ options, value, setValue, ...props }) => {
-  return (
-    <>
-      {options.map((option) => (
-        <label key={option}>
-          <input
-            type="radio"
-            value={option}
-            checked={value === option}
-            onChange={(e)=>{
-                setValue(e.target.value)
-            }}
-            {...props}
-          />
-          {option}
-        </label>
-      ))}
-    </>
-  );
-};
+// const Radio = ({ pergunta, options, onChange, value }) => {
+//   return (
+//     <fieldset style={{padding: '2rem', marginBottom:'1rem', border:'2px solid #eee'}}>
+//       <legend>
+//         <h2>{pergunta}</h2>
+//       </legend>
+//       {options.map((option) => (
+//         <label
+//           htmlFor=""
+//           key={option}
+//           style={{ marginBotton: "1rem", fontFamily: "monospace" }}
+//         >
+//           <input type="radio" checked={ value=== option} value={option} onChange={onChange}/>
+//           {option}
+//         </label>
+//       ))}
+//       <button>Proximo</button>
+//     </fieldset>
+//   );
+// };
 
-export default Radio;
+// export default Radio;
+
+
+import React from 'react'
+
+const Radio = (props) => {
+
+    if(props.show){
+        return (
+            <fieldset>
+                
+                <legend style={{fontWeight:"bold"}}>{props.title} {props.show? 'sim': 'nao'}</legend>
+                {props.options.map(option=>(
+                    <label htmlFor="" key={option}>
+                        <input type="radio" id={props.id} onChange={props.handleChange} value={option} checked={option === props.resposta}/>
+                        {option}
+                    </label>
+                ))}
+            </fieldset>
+        )
+    }else{
+        return null;
+    }
+
+    
+}
+
+export default Radio
